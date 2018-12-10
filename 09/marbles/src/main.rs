1f6071a955cfa173464c61a81c7094bc0251465e
David Wagner <mnem@noiseandheat.com>
2018-12-10 23:08:49 +0000

    Aha, Rust has ring buffers

--- 09/marbles/src/main.rs
+++ 09/marbles/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::collections::VecDeque;
 use regex::Regex;
 
 fn main() {
@@ -13,30 +14,20 @@ fn read_input() -> String {
         .expect("Failed to read file")
 }
 
-fn process(input: &str) -> i32 {
+fn process(input: &str) -> usize {
     let setup = GameSetup::from(input);
     let mut game = GameState::new(setup);
 
     game.play();
 
-    let (high_player, high_score) = game.highest_score();
+    let (_, high_score) = game.highest_score();
 
     return high_score;
 }
 
-fn modulus(a: i32, b: i32) -> i32 {
-    if a == 0 {
-        return 0;
-    } else if a > 0 {
-        return a % b;
-    } else {
-        return (a % b) + b;
-    }
-}
-
 struct GameSetup {
-    num_players: i32,
-    last_marble_score: i32,
+    num_players: usize,
+    last_marble_score: usize,
 }
 
 impl GameSetup {
@@ -45,38 +36,35 @@ impl GameSetup {
         let captures = re.captures(string).expect("Malformed game setup string.");
 
         let num_players = captures["players"].parse().expect("Malformed players number.");
-        let last_marble_score: i32 = captures["last_marble_score"].parse().expect("Malformed last marble score.");
+        let last_marble_score = captures["last_marble_score"].parse().expect("Malformed last marble score.");
 
-        GameSetup { num_players, last_marble_score, }
+        GameSetup { num_players, last_marble_score }
     }
 }
 
 struct GameState {
     setup: GameSetup,
-    player_scores: Vec<i32>,
-    placed_marbles: Vec<i32>,
-    current_marble_position: i32,
-    next_marble_score: i32,
+    player_scores: Vec<usize>,
+    placed_marbles: VecDeque<usize>,
 }
 
 impl GameState {
     fn new(setup: GameSetup) -> GameState {
-        let player_scores = vec![0; setup.num_players as usize];
-        let placed_marbles = vec![0];
-        let current_marble_position = 0;
-        let next_marble_score = 1;
+        let player_scores = vec![0usize; setup.num_players as usize];
+        let mut placed_marbles = VecDeque::new();
+        placed_marbles.push_front(0);
 
-        GameState { setup, player_scores, placed_marbles, current_marble_position, next_marble_score }
+
+        GameState { setup, player_scores, placed_marbles }
     }
 
     fn play(&mut self) {
-        while self.next_marble_score <= self.setup.last_marble_score {
-            self.place_next_marble(self.next_marble_score);
-            self.next_marble_score += 1;
+        for marble in 1..= self.setup.last_marble_score {
+            self.place_next_marble(marble);
         }
     }
 
-    fn place_next_marble(&mut self, next_marble_score: i32) {
+    fn place_next_marble(&mut self, next_marble_score: usize) {
         if next_marble_score != 0 && next_marble_score % 23 == 0 {
             self.place_23_marble(next_marble_score);
         } else {
@@ -84,48 +72,39 @@ impl GameState {
         }
     }
 
-    fn place_normal_marble(&mut self, marble: i32) {
-        let position = modulus(self.current_marble_position + 2, self.placed_marbles.len() as i32);
-        assert!(position >= 0);
-
-        self.placed_marbles.insert(position as usize, marble);
-        self.current_marble_position = position;
-    }
-
-    fn place_23_marble(&mut self, marble: i32) {
-        let player = self.next_marble_score % self.player_scores.len() as i32;
-        let position = modulus(self.current_marble_position - 7, self.placed_marbles.len() as i32);
-        assert!(position >= 0);
-
-        let taken_marble = self.placed_marbles.remove(position as usize);
-        let score = marble + taken_marble;
-        self.player_scores[player as usize] += score;
-
-        self.current_marble_position = position;
-    }
-
-    fn board_string(&self) -> String {
-        let mut board = String::new();
-        for (index, marble) in self.placed_marbles.iter().enumerate() {
-            if index == self.current_marble_position as usize {
-                board += &format!(" ({})", marble);
-            } else {
-                board += &format!(" {}", marble);
-            }
+    fn place_normal_marble(&mut self, marble: usize) {
+        // Rotate the buffer CW
+        for _ in 0..2 {
+            let v = self.placed_marbles.pop_front().expect("Can't rotate CW");
+            self.placed_marbles.push_back(v);
         }
-        return board;
+
+        self.placed_marbles.push_front(marble);
     }
 
-    fn highest_score(&self) -> (usize, i32) {
+    fn place_23_marble(&mut self, marble: usize) {
+        // Rotate the buffer CCW
+        for _ in 0..7 {
+            let v = self.placed_marbles.pop_back().expect("Can't rotate CCW");
+            self.placed_marbles.push_front(v);
+        }
+
+        let taken_marble = self.placed_marbles.pop_front().expect("Could not take marble");
+
+        let player = marble % self.player_scores.len();
+        self.player_scores[player] += marble + taken_marble;
+    }
+
+    fn highest_score(&self) -> (usize, usize) {
         let mut high_player = 0usize;
-        let mut high_score = -1;
+        let mut high_score = 0usize;
         for (player, score) in self.player_scores.iter().enumerate() {
             if *score > high_score {
                 high_player = player;
                 high_score = *score;
             }
         }
-        assert_ne!(-1, high_score);
+        assert_ne!(0, high_score);
         return (high_player, high_score);
     }
 }
@@ -149,9 +128,7 @@ mod tests {
         assert_eq!(9, result.setup.num_players);
         assert_eq!(25, result.setup.last_marble_score);
         assert_eq!(vec![0; 9], result.player_scores);
-        assert_eq!(vec![0; 1], result.placed_marbles);
-        assert_eq!(0, result.current_marble_position);
-        assert_eq!(1, result.next_marble_score);
+        assert_eq!(1, result.placed_marbles.len());
     }
 
     #[test]
